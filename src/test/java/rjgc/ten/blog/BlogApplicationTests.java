fb74bfcd2510492a096a981d6e520a66c15c8bfc
ASUS <2604452386@qq.com>
2023-03-31 20:41:34 +0800

    Initial commit

---
+++ src/test/java/rjgc/ten/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package rjgc.ten.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
