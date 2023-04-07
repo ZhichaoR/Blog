b24fa0d91602f3916adf8f6334a73765d6df72df
ASUS <2604452386@qq.com>
2023-04-07 18:13:05 +0800

    添加注释

--- src/main/java/rjgc/ten/blog/dao/CommentMapper.java
+++ src/main/java/rjgc/ten/blog/dao/CommentMapper.java
@@ -10,19 +10,30 @@ import rjgc.ten.blog.model.domain.Comment;
 import java.util.List;
 
 @Mapper
-
 public interface CommentMapper {
-//@Select("select * from t_comment")
+    //@Select("select * from t_comment")
 //    public List<Comment> queryComment();
+//    分页展示某个文章的评论
     @Select("select * from t_comment where article_id=#{aid} order by id DESC")
     public List<Comment> selectCommentWithPage(Integer aid);
+
+    //后台查询最新几条评论
     @Select("select * from t_comment order by id DESC")
     public List<Comment> selectNewComment();
-    @Insert("insert into t_comment(article_id,created,author,ip,content)"+
-    "Values (#{articleId},#{created},#{author},#{ip},#{content})")
+
+    //发表评论
+    @Insert("insert into t_comment(article_id,created,author,ip,content)" +
+            "Values (#{articleId},#{created},#{author},#{ip},#{content})")
     public void pushComment(Comment comment);
+
+    //站点服务统计，统计评论数量
     @Select("select count(1) from t_comment")
     public Integer countComment();//统计评论数量
+
+    //通过文章id删除评论信息
     @Delete("delete from t_comment where article_id=#{aid}")
     public void deleteCommentWithId(Integer aid);
 }
+
+
+//任智超：后续需要添加其他操作数据库的方法
