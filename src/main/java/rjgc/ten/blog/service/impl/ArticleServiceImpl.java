d7d184250a79674a9cf04fb2291b97a0309e6e2a
ASUS <2604452386@qq.com>
2023-04-07 18:05:46 +0800

    开启事务注解

--- src/main/java/rjgc/ten/blog/service/impl/ArticleServiceImpl.java
+++ src/main/java/rjgc/ten/blog/service/impl/ArticleServiceImpl.java
@@ -3,7 +3,9 @@ package rjgc.ten.blog.service.impl;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import rjgc.ten.blog.dao.ArticleMapper;
 import rjgc.ten.blog.dao.StatisticMapper;
 import rjgc.ten.blog.model.domain.Article;
@@ -14,16 +16,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class ArticleServiceImpl implements IArticleService {
     @Autowired
     private ArticleMapper articleMapper;
     @Autowired
     private StatisticMapper statisticMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-    @Override
-    public Article selectArticleWithId(Integer id) {
-        return null;
-    }
     //分页查询文章列表
     @Override
     public PageInfo<Article> selectArticleWithPage(Integer page, Integer count) {
@@ -38,18 +39,19 @@ public class ArticleServiceImpl implements IArticleService {
         PageInfo<Article> pageInfo = new PageInfo<>(articleList);
         return pageInfo;
     }
+
     //统计热度前十的文章信息
     @Override
     public List<Article> getHeatArticles() {
 
-        List<Statistic> list=statisticMapper.getStatistic();
-        List<Article> articlelist=new ArrayList<>();
-        for(int i=0;i<list.size();i++){
-            Article article=articleMapper.selectArticleWithId(list.get(i).getArticleId());
+        List<Statistic> list = statisticMapper.getStatistic();
+        List<Article> articlelist = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            Article article = articleMapper.selectArticleWithId(list.get(i).getArticleId());
             article.setHits(list.get(i).getHits());
             article.setCommentsNum(list.get(i).getCommentsNum());
             articlelist.add(article);
-            if(i>=9){
+            if (i >= 9) {
                 break;
             }
             return articlelist;
@@ -57,5 +59,21 @@ public class ArticleServiceImpl implements IArticleService {
 
 
         return null;
+    }
+
+
+    @Override
+    public Article selectArticleWithId(Integer id) {// 任智超：根据id查询单个文章详情，并使用redis缓存管理
+        Article article = null;
+        Object o = redisTemplate.opsForValue().get("article_" + id);
+        if (o != null) {
+            article = (Article) o;
+        } else {
+            article = articleMapper.selectArticleWithId(id);
+            if (article != null) {
+                redisTemplate.opsForValue().set("article_" + id, article);
+            }
+        }
+        return article;
     }
 }
