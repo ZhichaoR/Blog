1e20c893ddd402fb3bd51ef09b3c82791625e9d2
ASUS <2604452386@qq.com>
2023-04-10 08:21:40 +0800

    任智超:添加StatisticsBo类

---
+++ src/main/java/rjgc/ten/blog/model/ResponseDate/StatisticBo.java
@@ -0,0 +1,30 @@
+package rjgc.ten.blog.model.ResponseDate;
+
+public class StatisticBo {
+    private Integer articles;
+    private Integer comments;
+
+    public Integer getArticles() {
+        return articles;
+    }
+
+    public void setArticles(Integer articles) {
+        this.articles = articles;
+    }
+
+    public Integer getComments() {
+        return comments;
+    }
+
+    public void setComments(Integer comments) {
+        this.comments = comments;
+    }
+
+    @Override
+    public String toString() {
+        return "StaticticsBo{" +
+                "articles=" + articles +
+                ", comments=" + comments +
+                '}';
+    }
+}
