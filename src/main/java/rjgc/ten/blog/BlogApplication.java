95ee3545ff2aa0e28ff938d3f5013af78ba1c7d8
ASUS <2604452386@qq.com>
2023-04-11 10:49:52 +0800

    任智超:启动类更新

--- src/main/java/rjgc/ten/blog/BlogApplication.java
+++ src/main/java/rjgc/ten/blog/BlogApplication.java
@@ -1,11 +1,12 @@
 package rjgc.ten.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 
 @EnableCaching
+@MapperScan(value = "rjgc.ten.blog.dao")
 @SpringBootApplication
 public class BlogApplication {
 
