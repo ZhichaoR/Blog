f495a5e7bb8ec2487814a3df0010cb03182c6c2c
GuoziyunY <986187387@qq.com>
2023-04-11 14:55:16 +0800

    郭子昀：文章评论管理前台提交

--- src/main/java/rjgc/ten/blog/service/ICommentService.java
+++ src/main/java/rjgc/ten/blog/service/ICommentService.java
@@ -5,6 +5,9 @@ import rjgc.ten.blog.model.domain.Comment;
 
 //文章评论业务处理接口
 public interface ICommentService {
-    //    获取文章下的评论
+    //  任智超：  获取文章下的评论
     public PageInfo<Comment> getComments(Integer aid, int page, int count);
+
+    //    郭子昀：发表评论
+    public void pushComment(Comment comment);
 }
