1dc2184a5f743f6191c493ede0434fe51f4a2ac1
qin <1455322598@qq.com>
2023-04-17 10:43:29 +0800

    改了一个字

--- src/main/java/rjgc/ten/blog/web/scheduletask/Scheduletask.java
+++ src/main/java/rjgc/ten/blog/web/scheduletask/Scheduletask.java
@@ -21,13 +21,13 @@ public class Scheduletask {
     @Value("${spring.mail.username}")
     private String mailto;
     //定时邮件发送任务，通过注解指定每月1日中午12点调用邮件发送任务发送邮件
-    @Scheduled(cron="0 */3 * * * ?")
+    @Scheduled(cron="0 */1 * * * ?")
     public void sendEmail(){
         //定制邮件内容
         long totalvisit = statisticMapper.getTotalVisit();
         long totalComment = statisticMapper.getTotalComment();
         StringBuffer content = new StringBuffer();
-        content.append("博客系统总访问量未："+totalvisit+"人次").append("\n");
+        content.append("博客系统总访问量为："+totalvisit+"人次").append("\n");
         content.append("博客系统总评论量为："+totalComment+"人次").append("\n");
         mailUtils.sendSimpleEmail(mailto, "个人博客系统流量统计情况",content.toString());
     }
